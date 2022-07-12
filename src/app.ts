76670956de0a057dc4de8b0ec1788f042652733c
vascifrancca <vascofrancca@icloud.com>
2022-07-12 18:20:53 -0300

    Primeiro commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express,{ Application, Request, Response} from  'express';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req: Request,res: Response)=>{
+    res.send('Olá Mundo Espacial');
+ 
+});
+
+app.listen(port, ()=>{
+    console.log(`Fomos conectados com sucesso ${port}`)
+});
